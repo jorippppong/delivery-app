365858c598a07695ee1b02c1e3a7873ad8677fe3
jorippppong <duelee75@gmail.com>
2025-09-29 14:59:28 +0900

    first commit

---
+++ src/test/java/com/sparta/foodorder/FoodorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.foodorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodorderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
