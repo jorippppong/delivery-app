745b1dd73c78aa446ca9b63016d7b684155013ba
doraemong <k12u2138@gmail.com>
2025-10-01 12:44:51 +0900

    fix: 유저 테이블 명 수정

--- src/main/java/com/sparta/foodorder/domain/user/domain/User.java
+++ src/main/java/com/sparta/foodorder/domain/user/domain/User.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
+@Table(name = "p_user")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
