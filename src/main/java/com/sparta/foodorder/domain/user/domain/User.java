a436d6e5ab87791037581a5f456fcb325e64e046
Mybread2 <ckwnsgh4@naver.com>
2025-10-13 15:36:04 +0900

    refactor: UserRole 크기 수정
    SQL Error: 1265, SQLState: 01000
    Data truncated for column 'role' at row 1
    에러 해결

--- src/main/java/com/sparta/foodorder/domain/user/domain/User.java
+++ src/main/java/com/sparta/foodorder/domain/user/domain/User.java
@@ -35,11 +35,11 @@ public class User extends BaseEntity {
     private String userPhone;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private UserRole role = UserRole.USER;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private UserStatus status = UserStatus.ACTIVE;
 
     @Column(name = "is_deleted", nullable = false)
